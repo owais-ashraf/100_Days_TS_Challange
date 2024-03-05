a08817a1415cfe344e2ff373be3118c6dc8dbf48
Ozil <65316921+owais-ashraf@users.noreply.github.com>
2024-03-06 00:47:38 +0530

    Delete 04 Day/Assignment 12 directory

--- 04 Day/Assignment 12/ArrayMessage.js
+++
@@ -1,4 +0,0 @@
-var FriendsName = ["Ozil", "Asad", "Ali", "Fahad", "Saad"];
-for (var i = 0; i < FriendsName.length; i++) {
-    console.log("Hello", FriendsName[i], "You are invited for a Dinner");
-}
--- 04 Day/Assignment 12/ArrayMessage.ts
+++
@@ -1,4 +0,0 @@
-let FriendsName : string[] = ["Ozil","Asad","Ali","Fahad","Saad"];
-for (let i = 0; i<FriendsName.length; i++){
-    console.log("Hello", FriendsName[i], "You are invited for a Dinner");
-}
